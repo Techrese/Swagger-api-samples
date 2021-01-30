019729062b8111c1b0b315cff2aba8802c3d2baa
TechnologyResearcher <tim.clinckemalie@hotmail.com>
2021-01-30 15:32:52 +0100

    Added docker support Added entityframworkcore Changed target to .net 5.0 Added model Book

---
+++ Swagger-Samples/Swagger-Samples/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swagger_Samples.Models
+{
+    public class Book
+    {
+        public Guid Book_Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public string Genre { get; set; }
+
+        public int TotalAmountOfPages { get; set; }
+    }
+}
